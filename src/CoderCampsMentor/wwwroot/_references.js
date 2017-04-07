dab7e846fecc162b127bc29071429bec02571574
Mochammad Ibrahim <izzatullahibrahim@yahoo.com>
2017-04-06 21:41:09 -0700

    make the separate button for the edit profile page

--- src/CoderCampsMentor/wwwroot/_references.js
+++ src/CoderCampsMentor/wwwroot/_references.js
@@ -13,26 +13,6 @@
 /// <reference path="../bower_components/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
 /// <reference path="lib/Navbar.js" />
 /// <reference path="lib/videoCall.js" />
-<<<<<<< HEAD
-=======
-/// <reference path="lib/videocall.js" />
->>>>>>> 59e4b2a12ace46cdfcbd31a32d5f75b600fd99f1
-/// <reference path="ngapp/app.js" />
-/// <reference path="ngapp/controllers/accountcontroller.js" />
-/// <reference path="ngapp/controllers/applicationuserscontroller.js" />
-/// <reference path="ngapp/controllers/categoriescontroller.js" />
-/// <reference path="ngapp/controllers/controllers.js" />
-/// <reference path="ngapp/controllers/inboxcontroller.js" />
-/// <reference path="ngapp/controllers/messagecontroller.js" />
-/// <reference path="ngapp/controllers/messageinfocontroller.js" />
-/// <reference path="ngapp/controllers/profilecontroller.js" />
-/// <reference path="ngapp/controllers/subcategoriescontroller.js" />
-/// <reference path="ngapp/controllers/videocallcontroller.js" />
-/// <reference path="ngapp/services/accountservice.js" />
-/// <reference path="ngapp/services/commservice.js" />
-/// <reference path="ngapp/services/messageservice.js" />
-/// <reference path="ngapp/services/profileservice.js" />
-=======
 /// <reference path="ngApp/app.js" />
 /// <reference path="ngApp/controllers/accountController.js" />
 /// <reference path="ngApp/controllers/applicationUsersController.js" />
@@ -45,4 +25,3 @@
 /// <reference path="ngApp/services/CommService.js" />
 /// <reference path="ngApp/services/profileService.js" />
 /// <reference path="ngApp/services/services.js" />
->>>>>>> c9f27d86e6185e716119d8b0e92f36f79bbb0520
